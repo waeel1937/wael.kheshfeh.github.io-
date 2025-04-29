8c4cf64215fddcabee47ed1179ba254d06e04400
Wael Kheshfeh <40004361+waeel1937@users.noreply.github.com>
2025-04-29 09:05:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+// Hide Loading Spinner
+window.addEventListener("load", () => {
+  document.getElementById("loading-overlay").style.display = "none";
+});
+
+// Back to Top Button
+const topBtn = document.getElementById("topBtn");
+window.onscroll = function() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+topBtn.onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
